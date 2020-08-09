af37a713326fb8e6bbf00dd6fd95dee1580fece3
devansh259 <42205065+devansh259@users.noreply.github.com>
2020-08-09 18:06:04 +0530

    Update Ass1_Q1.cpp

--- Ass1_Q1.cpp
+++ Ass1_Q1.cpp
@@ -2,14 +2,14 @@
 
 int main() {
     int s=0,m=0,n=0,t=0;
-    printf("Integer input:");
+    printf("Enter any integer");
     scanf("%d", &n);
    if (n >= 0){
        int i;
         for(i=1;i<=n;++i){
             s +=i;
         }
-        printf("Sum of integers till the input:%d", s);
+        printf("Sum of integers till the number:%d", s);
    }
    else{
        m=-1*n;
@@ -18,7 +18,7 @@ int main() {
             s +=i;
         }
         t=-1*s;
-        printf("Sum of integers till the input:%d", t);
+        printf("Sum of integers till the number:%d", t);
     }
     return 0;
 }
