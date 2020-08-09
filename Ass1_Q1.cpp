29ca61c6c1009d5efcbe080f07cac732e6e36b4d
devansh259 <42205065+devansh259@users.noreply.github.com>
2020-08-09 21:35:41 +0530

    Update Ass1_Q1.cpp

--- Ass1_Q1.cpp
+++ Ass1_Q1.cpp
@@ -2,7 +2,7 @@
 int main() {
     int s =0, i;
     for (i = 1; i <= 100; ++i) {
-        // condition for non-prime
+        
         s=s+i;
     }
     printf("Sum of numbers from 1 to 100 = %d" , s);
