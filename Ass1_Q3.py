17133befb0110e064972464ad1bd539f96998b97
devansh259 <42205065+devansh259@users.noreply.github.com>
2020-08-09 19:53:46 +0530

    Update Ass1_Q3.py

--- Ass1_Q3.py
+++ Ass1_Q3.py
@@ -1,12 +1,19 @@
+n=int(input("Enter the number of terms to find sum "))
 x=True
 i=1
 sum=1
 pre=0
-while(x==True):
-    sum=sum+1/(i+1)
-    pre=pre+1/i
-    if((sum-pre)<0.001):
-        x=False
+if(n<=0):
+    print("enter positive integers only")
+else:
+    while(x==True ):
+        sum=sum+1/(i+1)
+        pre=pre+1/i
+        if((sum-pre)<0.001)|(i==n):
+            x=False
+        else:
+            i=i+1
+    if(x==True):
+        print("sum doesn't change by 0.001 and sum=" + str(sum))
     else:
-        i=i+1
-print(sum)
+        print("sum for " + str(n) + " terms=" +str(sum))
