7023efd590b28ad922df647c3d683ce5ff69f015
devansh259 <42205065+devansh259@users.noreply.github.com>
2020-08-09 18:02:02 +0530

    Update Ass1_Q1.py

--- Ass1_Q1.py
+++ Ass1_Q1.py
@@ -1,4 +1,11 @@
+n=int(input("Enter any integer"))
 s=0
-for i in range(101):
-    s=s+i
-print("sum of 1 to 100 =" + str(s))
+if(n>=0):
+    for i in range(n):
+        s=s+i
+    print("sum upto to the number=" + str(s))
+else:
+    for i in range(-n):
+        s=s+i
+    print("sum upto to the number=" + str(-s))
+        
