73419e193702f08aa453cc295146c462ef01f5da
devansh259 <42205065+devansh259@users.noreply.github.com>
2020-08-09 21:30:59 +0530

    Update Ass1_Q1.py

--- Ass1_Q1.py
+++ Ass1_Q1.py
@@ -1,9 +1,5 @@
-n=int(input("Enter any natural number"))
+
 s=0
-if(n>1):
-    for i in range(n):
-        s=s+i
-    print("sum upto to the number=" + str(s))
-else:
-    print("please enter positive number greater than 1")
-        
+for i in range(101):
+    s= s+i
+print("The sum from 1 to 100 = " + str(s))
