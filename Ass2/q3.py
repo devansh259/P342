f1d5586ba79020284be3fff3d19ffc0a3001ab90
devansh259 <42205065+devansh259@users.noreply.github.com>
2020-08-18 01:36:12 +0530

    Create q3.py

---
+++ Ass2/q3.py
@@ -0,0 +1,24 @@
+m=[[1,2,3],
+   [4,5,6],
+   [7,8,9]] 
+n=[[-1,2,7],
+   [9,-5,6],
+   [7,8,-9]]
+
+a=[2,5,7]
+b=[]
+p=[[0,0,0],[0,0,0],[0,0,0]]
+for row in m:
+	sum=0
+	for i in range(3):
+		sum+=row[i]*a[i]
+	b.append(sum)
+
+for i in range(len(m)):
+	for j in range(len(n[0])):
+		for k in range(len(n)):
+			p[i][j]+= m[i][k] * n[k][j]
+
+print("Product of A and M = " + str(b))
+print("Product of M and N = " + str(p))
+	
